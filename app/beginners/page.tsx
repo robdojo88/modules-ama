b6e7454e7e9adfa128a849b34b8c1683ebff99ed
robdojo88 <robert.dojo.88@gmail.com>
2025-10-03 09:12:46 +0800

    second commit

--- app/beginners/page.tsx
+++ app/beginners/page.tsx
@@ -3,7 +3,9 @@ import Card from '@/components/cards';
 export default function Beginners() {
     return (
         <section>
-            <h1 className='text-2xl font-bold'>Beginners</h1>
+            <h1 className='text-center mb-2 md:mb-5 text-2xl font-bold'>
+                Beginners
+            </h1>
             <div>
                 <Card />
             </div>
